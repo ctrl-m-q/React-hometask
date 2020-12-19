30040db96cc4ceb18183a0c88c322ef77ae08d4f
ctrl-m-q <64306905+ctrl-m-q@users.noreply.github.com>
2020-12-19 19:49:26 +0200

    initial commit

---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Form, Input, Button} from 'antd';
+import { render } from '@testing-library/react';
+
+
+
+const  {Item}  = Form;
+
+export const TodoForm = (props) => {
+    const {onSubmit} = props;
+    
+    const finish = (values) => {
+        console.log('these are the values')
+        console.log(values)
+        onSubmit(values.name, values.title, values.description)
+    }
+
+    const fieldsValidationMessage = "the name should not be lesss than 3 characters!";
+    const titlecapValidationmessage = "the title must begin with a capital letter!";
+    
+
+
+    return(
+        <Form className={'todo-form'} layout={'inline'} onFinish={finish}>
+            <Item name = {'name'} label="Name">
+                <Input
+                minLength = {3}
+                validationMessage = {fieldsValidationMessage} 
+                />
+            </Item>
+            <Item name = {'title'} label = "Title">
+                <Input 
+                minLength = {3}
+                pattern = {"^[A-Z][A-Za-z]+"}
+                validationMessage = {titlecapValidationmessage}
+                />
+            </Item>
+                <Item name = {'description'} label = "Description">
+                    <Input 
+                    minLength = {3}
+                    validationMessage = {fieldsValidationMessage}
+                    />
+                </Item>
+
+            <Item>
+                <Button htmlType = {'submit'}>Add</Button>
+            </Item>
+
+
+            
+        </Form>
+    )
+
+    
+
+    
+    
+}
+
+
---
+++ src/components/Todoitem.jsx
@@ -0,0 +1,28 @@
+import React, { useState} from 'react';
+import {Button, Checkbox } from 'antd';
+import {DeleteFilled} from '@ant-design/icons';
+
+
+export const Todoitem = (props) => {
+    const { item, onCheck, onRemove } = props;
+
+    const check = () =>{
+        onCheck(item.id);
+    }
+
+    const Remove = () =>{
+        onRemove(item.id);
+    }
+    
+    return(
+        
+        <li className = "Todo-item" key = {item.id}>
+            <Checkbox classname = "checkbox" checked = {item.checked} onChange= {check} > {item.name}, {item.title}, {item.description}
+            </Checkbox>
+            <Button style = {{background:"#f5222d"}} onClick={Remove} icon = {<DeleteFilled />} ></Button> 
+        </li>
+        
+            
+    )
+    
+}
